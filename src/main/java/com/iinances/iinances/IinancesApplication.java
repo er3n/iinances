3e0e0ab4605406f818d628d61b17d27f0be4f42f
er3n <erenozturk@gmail.com>
2018-01-02 22:35:07 +0300

    first commit

---
+++ src/main/java/com/iinances/iinances/IinancesApplication.java
@@ -0,0 +1,12 @@
+package com.iinances.iinances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IinancesApplication.class, args);
+	}
+}
